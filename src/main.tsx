4e8766c5ad8e2fbdf5c384a8365ba3c0424e9b64
Alvaro Tomasino <atomasino08@gmail.com>
2025-08-18 20:38:25 -0300

    directories structure + react routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </StrictMode>
   )
 } else {
   throw new Error('Root element not found')
+  console.log('Root element not found')
 }
