fa13aacc48ce28cf8af76cf4fbf029183b9a4033
Alvaro Tomasino <atomasino08@gmail.com>
2025-08-21 16:28:57 -0300

    revisar app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import { MainNavbar } from './navigation/MainNavbar.tsx'
 import { Home } from './screens/home.tsx'
 import { ShowTiposAnomalias } from './components/showTiposAnomalias.tsx'
-import { createBrowserRouter, createRoutesFromElements, Route, Routes } from 'react-router'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Routes,
+} from 'react-router'
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      
-    )
-  return (
-    <>
-      <main>
-        <MainNavbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/show-tipo-anomalia" element={<ShowTiposAnomalias />} />
-        </Routes>
-      </main>
-    </>
-  )
+  ;<>
+    <main>
+      <MainNavbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/show-tipo-anomalia" element={<ShowTiposAnomalias />} />
+      </Routes>
+    </main>
+  </>
 }
 
 export default App
