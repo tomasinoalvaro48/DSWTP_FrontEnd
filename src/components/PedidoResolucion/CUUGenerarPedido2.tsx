eca0511b46c166a08c362bbae81c8f3ce56eb462
Alvaro Tomasino <alvaroyaugusto@gmail.com>
2025-10-03 00:35:11 -0300

    casi terminado

--- src/components/PedidoResolucion/CUUGenerarPedido2.tsx
+++ src/components/PedidoResolucion/CUUGenerarPedido2.tsx
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import { useNavigate, useLocation, Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useLocation, Link } from 'react-router-dom'
 import type { TipoAnomalia, PedidoResolucion, Anomalia } from '../../entities/entities.ts'
 import { get } from '../../api/dataManager.ts'
 
 export function GenerarPedidoPaso2() {
-  const navigate = useNavigate()
-  const location = useLocation()
   const [anoms, setAnoms] = useState<Anomalia[]>([])
-  const { data: tipos_anomalia, loading, error } = get<TipoAnomalia>('tipo_anomalia')
+  const { data: tipos, loading, error } = get<TipoAnomalia>('tipo_anomalia')
+
+  // Datos del paso 1
+  const location = useLocation()
   const state = location.state as PedidoResolucion
 
-  const handleAddAnomalia = () => {
-    const tipoSeleccionado = tipos_anomalia?.find((t) => t.id === e.)
-    if (!tipoSeleccionado) return
-    setAnoms([...anoms, tipoSeleccionado])
-    setAnomaliaInput('')
+  const handleAddAnomalia = (t: TipoAnomalia) => {
+    const nuevaAnomalia: Anomalia = {
+      id: '',
+      tipo_anomalia: t,
+      resultado_anomalia: 'inconcluso',
+      pedido_resolucion: state,
+    }
+    if (anoms.map((a) => a.tipo_anomalia.id).includes(t.id)) return alert('Anomalía ya agregada')
+    setAnoms([...anoms, nuevaAnomalia])
   }
 
-  const handleSiguiente = () => {
-    state.anomalias = anoms
-    navigate('/generar-pedido-paso-3', { state: location.state })
+  const handleDeleteAnomalia = (id: string) => {
+    setAnoms(anoms.filter((a) => a.id !== id))
+  }
+
+  const handleFinish = () => {
+    if (anoms.length === 0) return alert('Debe agregar al menos una anomalía')
   }
 
   return (
@@ -27,41 +35,55 @@ export function GenerarPedidoPaso2() {
       <h1>Generar Pedido - Paso 2</h1>
 
       {loading && <p>Cargando tipos de anomalía...</p>}
-      {error && <p>Error al cargar tipos de anomalías.</p>}
-      {!loading && !error && tipos_anomalia.length > 0 && (
-        <form onSubmit={handleAddAnomalia} className="mb-9">
-          <label htmlFor="anomalia">Tipo de Anomalía</label>
-          <select
-            className="form-select"
-            id="anomalia"
-            name="anomalia"
-            value={anomaliaInput}
-            onChange={(e) => setAnomaliaInput(e.target.value)}
-            required
-          >
-            <option value="">Selecciona una anomalía</option>
-            {tipos_anomalia?.map((tipos) => (
-              <option key={tipos.id} value={tipos.id}>
-                {tipos.nombre_tipo_anomalia}
-              </option>
-            ))}
-          </select>
-
-          <button type="submit" className="btn btn-primary mt-2">
-            Agregar anomalía
-          </button>
-        </form>
+      {!loading && error && <p>Error al cargar tipos de anomalías.</p>}
+      {!loading && !error && tipos.length > 0 && (
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Anomalia</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {tipos.length === 0 ? (
+              <p>No hay entidades cargadas</p>
+            ) : (
+              tipos.map((t) => (
+                <tr key={t.id}>
+                  <th>{t.nombre_tipo_anomalia}</th>
+                  <th>
+                    <button
+                      type="submit"
+                      className="btn btn-primary mt-2"
+                      onClick={() => handleAddAnomalia(t)}
+                    >
+                      Agregar anomalía
+                    </button>
+                  </th>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
       )}
-
-      <h3>Anomalías cargadas:</h3>
-      <ul className="list-group mb-3">
-        {anomalias.map((a, idx) => (
-          <li key={idx} className="list-group-item">
-            {a.tipo_anomalia.nombre_tipo_anomalia}
-          </li>
-        ))}
-      </ul>
-
+      <table className="table table-bordered">
+        <thead>
+          <h3>Anomalías cargadas:</h3>
+        </thead>
+        <tbody>
+          {anoms.map((a) => (
+            <tr>
+              <th key={a.id}>{a.tipo_anomalia.nombre_tipo_anomalia}</th>
+              <th>
+                <button className="btn btn-danger" onClick={() => handleDeleteAnomalia(a.id)}>
+                  {' '}
+                  Eliminar
+                </button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <label htmlFor="descripcion" className="form-label">
         Descripcion (Opcional)
       </label>
@@ -73,11 +95,11 @@ export function GenerarPedidoPaso2() {
         placeholder="Ingrese una decripcion (Opcional)"
       />
 
-      <button className="btn btn-primary" onClick={handleSiguiente}>
-        Siguiente
+      <button className="btn btn-primary" onClick={handleFinish}>
+        Realizar Pedido
       </button>
       <Link className="btn btn-secondary" to="/show-pedido">
-        Cancelar
+        Cancelar Pedido
       </Link>
     </div>
   )
