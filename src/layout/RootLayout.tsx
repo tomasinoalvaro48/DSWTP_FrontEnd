00c3fbd6d5132e65a2d89d257f0eef9a388d2e76
meliaronson <melinaaronson@gmail.com>
2025-10-16 19:32:52 -0300

    footer en pantallas principales

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,13 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { MainNavbar } from '../navigation/MainNavbar.tsx'
 import Footer from '../footer/Footer.tsx'
 
 export function RootLayout() {
+  const location = useLocation()
+  const mostrarFooter = location.pathname === '/' || location.pathname === '/nav-map' || location.pathname === '/politicas'
+
   return (
     <div>
       <MainNavbar />
       <Outlet />
-      <Footer />
+      {mostrarFooter && <Footer />}
     </div>
   )
 }
