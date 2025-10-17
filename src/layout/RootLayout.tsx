98847f177368c5ff7312567c5042a4b2dfbc4741
Tomas <tomas.aguilera.27@gmail.com>
2025-10-17 09:59:11 -0300

    marge footer

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,26 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { MainNavbar } from '../navigation/MainNavbar.tsx'
 import Footer from '../footer/Footer.tsx'
 
 export function RootLayout() {
   const location = useLocation()
   const mostrarFooter =
-    location.pathname === '/'
-    || location.pathname === '/nav-map'
-    || location.pathname === '/modificar-perfil'
-    || location.pathname === '/politicas'
-    || location.pathname === '/mostrar-posibles-pedidos'
-    || location.pathname === '/show-pedidos-agregacion'
-    || location.pathname === '/show-mis-pedidos'
-    || location.pathname === '/show-mis-pedidos-denunciante'
-    || location.pathname === '/show-tipo-anomalia'
-    || location.pathname === '/show-denunciante'
-    || location.pathname === '/show-localidad'
-    || location.pathname === '/show-zona'
-    || location.pathname === '/show-usuario'
-    || location.pathname === '/show-pedido'
-    || location.pathname === '/tomar-pedidos-agregacion'
-    || location.pathname === '/approve-usuario'
+    location.pathname === '/' ||
+    location.pathname === '/nav-map' ||
+    location.pathname === '/modificar-perfil' ||
+    location.pathname === '/politicas' ||
+    location.pathname === '/mostrar-posibles-pedidos' ||
+    location.pathname === '/show-pedidos-agregacion' ||
+    location.pathname === '/show-mis-pedidos' ||
+    location.pathname === '/show-mis-pedidos-denunciante' ||
+    location.pathname === '/show-tipo-anomalia' ||
+    location.pathname === '/show-denunciante' ||
+    location.pathname === '/show-localidad' ||
+    location.pathname === '/show-zona' ||
+    location.pathname === '/show-usuario' ||
+    location.pathname === '/show-pedido' ||
+    location.pathname === '/tomar-pedidos-agregacion' ||
+    location.pathname === '/approve-usuario'
 
   return (
     <div>
